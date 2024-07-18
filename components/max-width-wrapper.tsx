f959c32c213d4f99085fb1398a6b283a50ad2e9a
Jay <joelogira@gmail.com>
2024-07-18 23:56:18 +0300

    Created max-width-wrapper component

---
+++ components/max-width-wrapper.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+
+type MaxWidthWrapperProps = {
+    className?: string,
+    children: React.ReactNode
+}
+
+export default function MaxWidthWrapper({ className, children }: MaxWidthWrapperProps) {
+    return (
+        <div className={cn(
+            "h-full mx-auto w-full w-screen-xl px-2.5 md:px-20",
+            className
+        )}>
+            {children}
+        </div>
+    )
+}
