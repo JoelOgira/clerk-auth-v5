9d807c992a782f9e60719da2447c5214e179944b
Jay <joelogira@gmail.com>
2024-07-19 18:06:29 +0300

    Updated the styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/theme-provider"
+import Footer from "@/components/footer"
+import Header from "@/components/header"
 import "./globals.css"
-
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,7 +21,20 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="flex flex-col h-screen">
+              <Header />
+              {children}
+              <Footer />
+            </div>
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
